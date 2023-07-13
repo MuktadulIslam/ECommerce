f85a0e045ba7c2c49de777c3d11c851898fbb594
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-07-13 10:37:28 +0600

    successfully completed mobile menu

---
+++ ecommerce/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import Header2 from './Header2'
+
+export default function HomePage() {
+    return (
+        <>
+            <Header2/>
+        </>
+
+    )
+}
